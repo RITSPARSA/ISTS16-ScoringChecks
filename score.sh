c2e241d35323eb0006497a695570a8b1bc371b1a
Doshmajhan <cpc1007@g.rit.edu>
2018-02-03 07:31:45 -0500

    fixing smb check

--- score.sh
+++ score.sh
@@ -7,11 +7,8 @@ SMB_IP_1="10.2.$1.20"
 SMB_IP_2="10.2.$1.30"
 SMB_IP_3="10.2.$1.40"
 FTP_IP="10.2.$1.20"
-FTP_PORT="21"
 SSH_IP="10.3.$1.10"
-SSH_PORT="22"
 ELASTIC_IP="10.2.$1.50"
-ELASTIC_PORT="9000"
 DNS_IP="10.2.$1.40"
 
 # VARIABLES
@@ -24,19 +21,23 @@ SSH_COMMANDS="echo hi"
 ELASTIC_INDEX=""
 ELASTIC_DOCTYPE=""
 BASE_DN="dc=team$1,dc=ists"
+HASH="b73a0892cea768d727f1144c1455a40aa57e9b112145e6a20a56ed1aee607e90"
 
 # CHECKS
-python checks/comcenter/check.py -a $COMM_IP
-python checks/ecomm/check.py -t 1 -a $ECOMM_IP
-python checks/jenkins/check.py $JENKINS_SWIFT_IP $PASSWORD
-python checks/jenkins/check.py $JENKINS_WIN_IP $PASSWORD
-python checks/ftp/check.py $FTP_IP $FTP_PORT $WIN_USERNAME $PASSWORD $REMOTE_FILE_PATH $REMOTE_FILE_CONTENTS
-python checks/ssh/check.py $SSH_IP $SSH_PORT $LINUX_USERNAME $PASSWORD "$SSH_COMMANDS"
-#python checks/elastic/check.py $ELASTIC_IP $ELASTIC_PORT $ELASTIC_INDEX $ELASTIC_DOCTYPE
+#python checks/comcenter/check.py -a $COMM_IP
+#python checks/ecomm/check.py -t 1 -a $ECOMM_IP
+#python checks/jenkins/check.py $JENKINS_SWIFT_IP $PASSWORD
+#python checks/jenkins/check.py $JENKINS_WIN_IP $PASSWORD
+#python checks/ftp/check.py $FTP_IP 21 $WIN_USERNAME $PASSWORD $REMOTE_FILE_PATH $REMOTE_FILE_CONTENTS
+python checks/ssh/check.py $SSH_IP 22 $LINUX_USERNAME $PASSWORD "$SSH_COMMANDS"
+#python checks/elastic/check.py $ELASTIC_IP 9000 $ELASTIC_INDEX $ELASTIC_DOCTYPE
+python checks/smb/check.py --host $SMB_IP_1 --user $WIN_USERNAME --pass $PASSWORD --hash $HASH
+python checks/smb/check.py --host $SMB_IP_2 --user $WIN_USERNAME --pass $PASSWORD --hash $HASH
+python checks/smb/check.py --host $SMB_IP_3 --user $WIN_USERNAME --pass $PASSWORD --hash $HASH
 
 nslookup team$1.ists $DNS_IP >> /dev/null; [ $? = 0 ] && echo good || echo bad
 
-ldapsearch -x -h $DNS_IP -p 389 -D $WIN_USERNAME@team$1.ists -b "$BASE_DN" -w $PASSWORD '(objectclass=User)' cn  >> /dev/null; [ $? = 0 ] && echo good || echo bad
+#ldapsearch -x -h $DNS_IP -p 389 -D $WIN_USERNAME@team$1.ists -b "$BASE_DN" -w $PASSWORD '(objectclass=User)' cn  >> /dev/null; [ $? = 0 ] && echo good || echo bad
 
 
 # PINGS
--- smbcheck/smb_check.py
+++ smbcheck/smb_check.py
@@ -18,7 +18,7 @@ def check_smb(host, share_name, file_name, username, password, hash_to_test):
                     with open(temp_file.name, 'r') as read_temp_file:
                         data = read_temp_file.read()
                         hash_obj = hashlib.sha256(data.encode())
-                        if hash_obj.hexdigest == 'temp':
+                        if hash_obj.hexdigest() == 'b73a0892cea768d727f1144c1455a40aa57e9b112145e6a20a56ed1aee607e90':
                             print('SUCCESS')
                             return
     except Exception as e:
@@ -32,7 +32,7 @@ def main():
     parser.add_argument('--pass', help='password', dest='passw')
     parser.add_argument('--hash', help='hash for comparison of file to endpoint', dest='hash')
     args = parser.parse_args()
-    check_smb(args.host, 'space_rocks', 'test.txt', args.user, args.passw, args.hash)
+    check_smb(args.host, 'space_rocks', 'last_contact.txt', args.user, args.passw, args.hash)
 
 if __name__ == '__main__':
     main()
